5b57099d61f42f91c8980ea409d10e85b695ff70
Md. Fayjur Rahman <sourov.gm@gmail.com>
2018-08-09 22:37:44 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,386 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Festaprintwholesale.com</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/freelancer.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <link href="css/style.css" rel="stylesheet">
+
+
+    <link rel="shortcut icon" href="img/favicon.ico">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+
+    <!-- Navigation -->
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="#page-top">festaprintwholesale</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#portfolio">Products</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#about">About</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <img class="img-responsive" src="img/logo.png" alt="">
+                    <div class="intro-text">
+                        <span class="name">Festaprint</span>
+                        <hr class="star-light">
+                        <span class="skills">Apparels - Canvas Arts - Blinds & Shades - Home Textile - Loop Curtains - Doormat & Yogamat</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Product Grid Section -->
+    <section id="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Products</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal1" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <h1>Apparels</h1>
+                            </div>
+                        </div>
+                        <img src="img/products/apparels.jpg" class="img-responsive" alt="">
+                    </a>
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal2" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <h1>Canvas Arts</h1>
+                            </div>
+                        </div>
+                        <img src="img/products/canvas.jpg" class="img-responsive" alt="">
+                    </a>
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal3" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <h1>Blinds & Shades</h1>
+                            </div>
+                        </div>
+                        <img src="img/products/blinds.jpg" class="img-responsive" alt="">
+                    </a>
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal4" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <h1>Home Textile</h1>
+                            </div>
+                        </div>
+                        <img src="img/products/home_textile.jpg" class="img-responsive" alt="">
+                    </a>
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal5" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <h1>Loop Curtains</h1>
+                            </div>
+                        </div>
+                        <img src="img/products/loop_curtains.jpg" class="img-responsive" alt="">
+                    </a>
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal6" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <h1>Doormat & Yogamat</h1>
+                            </div>
+                        </div>
+                        <img src="img/products/yogamat2.jpg" class="img-responsive" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- About Section -->
+    <section class="success" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>About</h2>
+                    <hr class="star-light">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-2">
+                    <p>Festaprint founded in181 S.Whitfield Street Unit 1 Nazareth PA 18064 United States. We provide lots of high quality printed products.</p>
+                </div>
+                <div class="col-lg-4">
+                    <p>We collect world's best quality products for Festaprint. Before printing products, we always check products quality. We have a team who reviews our products regularly.</p>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section -->
+
+
+    <!-- Contact Section -->
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Contact Us</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+
+                <div class="row">
+                    <div class="col-md-3"></div>
+
+                    <div class="col-md-7">
+
+                        <?php
+
+                        $error_message = [];
+                        $success_message = '';
+
+                        $email = isset($_POST['email']) ? strip_tags($_POST['email']) : '';
+                        $name = isset($_POST['name']) ? strip_tags($_POST['name']) : '';
+                        $message = isset($_POST['message']) ? strip_tags($_POST['message']) : '';
+
+                        if(isset($_POST['contact-form'])) {
+
+                            echo '<script>$(function(){$("html, body").animate({scrollTop: $("#contact-form-row").offset().top + 5}, 100);});</script>';
+
+                            if($email == '' || $email == null) {
+                                $error_message['email'] = 'Email is required';
+                            }
+                            if($message == '' || $message == null) {
+                                $error_message['message'] = 'Message is required';
+                            }
+
+
+                            # if email or message is blank, show message to fill up required input
+
+                            # if email, and msg given (or, error_message is empty)
+                            # send email
+                            # if can send mail
+                            # show success message
+                            # show failed message
+
+
+                            if(empty($error_message)) {
+
+                                $to = '';
+
+                                if(isset($_GET['test-mail'])) {
+                                    $to = $_GET['test-mail'];
+                                } else {
+                                    $to = 'sourov.gm@gmail.com';
+                                }
+
+                                $subject = 'PiCanvas contact message';
+                                $msg = '<p>Email: ' . $email . '<p>'
+                                    . '<p>Name: ' . $name . '<p>'
+                                    . '<p>Message: ' . $message .'<p>';
+
+                                $headers = "MIME-Version: 1.0\n" ;
+                                $headers .= "Content-Type: text/html; charset=\"iso-8859-1\"\n";
+
+                                $sent = mail($to, $subject, $msg, $headers);
+
+                                if($sent) {
+                                    $success_message = '<div class=" alert alert-success">Message sent.</div><br>';
+                                } else {
+                                    $success_message = '<div class="alert alert-warning">Message not sent. Please try again.</div>';
+                                }
+                            }
+
+
+                        }
+
+                        function get_error_class($msg, $name) {
+
+                            if( isset($msg[$name]) ) {
+                                return ' has-error';
+                            }
+                            return '';
+                        }
+
+                        function get_error_message($msg, $name) {
+                            if( isset($msg[$name]) ) {
+                                return '<span class="text-danger">' . $msg[$name] . '</span>';
+                            }
+                            return '';
+                        }
+
+                        //                var_dump($error_message);
+                        ?>
+
+<!--                        <h3>Leave us a message:</h3>-->
+
+                        <form class="form-horizontal" role="form" action="" method="post">
+
+
+                            <div class="form-group<?php echo get_error_class($error_message, 'email'); ?>">
+                                <!--                    <label for="inputEmail1" class="col-md-4 control-label"></label>-->
+                                <input type="hidden" name="contact-form" value="yes">
+                                <div class="col-md-10">
+
+                                    <?php echo $success_message; ?>
+
+                                    <input name="email" value="<?php echo $email; ?>" type="email" class="form-control" id="inputEmail1" placeholder="Email">
+                                    <?php echo get_error_message($error_message, 'email'); ?>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <!--                    <label for="text1" class="col-md-4 control-label"></label>-->
+                                <div class="col-md-10">
+                                    <input name="name" type="text" value="<?php echo $name; ?>" class="form-control" id="text1" placeholder="Your Name">
+                                </div>
+                            </div>
+                            <div class="form-group<?php echo get_error_class($error_message, 'message'); ?>">
+                                <!--                    <label for="text1" class="col-md-4 control-label"></label>-->
+                                <div class="col-md-10">
+                                    <textarea name="message" id="message-text" class="form-control" placeholder="Message"><?php echo $message; ?></textarea>
+                                    <?php echo get_error_message($error_message, 'message'); ?>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-10">
+                                    <button type="submit" class="btn btn-success">Send</button>
+                                </div>
+                            </div>
+                        </form><!-- form -->
+
+                    </div><!-- col -->
+                    <!--        <div class="col-md-2"></div>-->
+                </div>
+        </div>
+    </section>
+
+
+
+    <!-- Contact Section -->
+
+    <!-- Footer -->
+    <footer class="text-center">
+        <div class="footer-above">
+            <div class="container">
+                <div class="row">
+                    <div class="footer-col col-md-4">
+                        <h3>Location</h3>
+                        <p>181 S.Whitfield Street Unit 1
+                            <br>Nazareth PA 18064 United States</p>
+                    </div>
+                    <div class="footer-col col-md-4">
+
+                    </div>
+                    <div class="footer-col col-md-4">
+                        <h3>Email</h3>
+                        <p>info@festaprintwholesale.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-below">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        Copyright &copy; festaprintwholesale.com 2016
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
+        <a class="btn btn-primary" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
+
+    <!-- Portfolio Modals -->
+
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/freelancer.min.js"></script>
+
+</body>
+
+</html>
